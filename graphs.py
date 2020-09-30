ba301ce81646038c4567d624bcd3c7836401fe32
florianthalmann <florianthalmann@bluewin.ch>
2020-09-30 17:43:20 +0900

    basic alignment graph reduction

--- graphs.py
+++ graphs.py
@@ -7,10 +7,11 @@ from graph_tool.topology import label_components
 from graph_tool.spectral import adjacency
 from graph_tool.inference.blockmodel import BlockState
 from graph_tool.topology import transitive_closure, all_paths, max_cliques
+from graph_tool.util import find_edge
 from util import group_adjacent, plot, plot_matrix
 
-def adjacency_matrix(graph):
-    return adjacency(graph).toarray().T
+def adjacency_matrix(graph, weight=None):
+    return adjacency(graph, weight).toarray().T
 
 def prune_isolated_vertices(g):
     not_isolated = g.new_vertex_property("bool")
@@ -34,71 +35,114 @@ def alignment_graph(lengths=[], pairings=[], alignments=[]):
     seq_index.a = np.concatenate([np.repeat(i,l) for i,l in enumerate(lengths)])
     time.a = np.concatenate([np.arange(l) for l in lengths])
     #add edges (alignments)
+    alm_index = g.new_edge_property("int")
+    seg_index = g.new_edge_property("int")
     for i,a in enumerate(alignments):
         if len(a) > 0:
             j, k = pairings[i]
             pairs = np.concatenate(a, axis=0)
             indicesJ = (np.arange(lengths[j]) + sum(lengths[:j]))[pairs.T[0]]
             indicesK = (np.arange(lengths[k]) + sum(lengths[:k]))[pairs.T[1]]
-            g.add_edge_list(np.vstack([indicesJ, indicesK]).T)
+            seg_indices = np.concatenate([np.repeat(i, len(a))
+                for i,a in enumerate(a)])
+            g.add_edge_list(np.vstack([indicesJ, indicesK,
+                np.repeat(i, len(pairs)), seg_indices]).T,
+                eprops=[alm_index, seg_index])
     #g.add_edge_list([(b, a) for (a, b) in g.edges()])
     print('created alignment graph', g)
     #g = prune_isolated_vertices(g)
     #print('pruned alignment graph', g)
     #g = transitive_closure(g)
     #graph_draw(g, output_size=(1000, 1000), output="results/casey_jones_bars.pdf")
-    return g, seq_index, time
+    return g, seq_index, time, alm_index, seg_index
 
 #remove all alignments that are not reinforced by others from a simple a-graph
-def clean_up(g, time, self_alignment):
-    plot_matrix(np.triu(adjacency_matrix(g)), "results/clean0.png")
+def clean_up(g, time, seg_index):
+    #plot_matrix(np.triu(adjacency_matrix(g)), "results/clean0.png")
     graph_draw(g, output_size=(1000, 1000), output="results/clean_up0.pdf")
-    segs = sorted(self_alignment, key=lambda a: len(a), reverse=True)
     reduced = Graph(directed=False)
     reduced.add_vertex(len(g.get_vertices()))
-    votes = []
-    for v in g.get_vertices():
-        n = g.get_out_neighbors(v)
+    edge_combos = []
+    #for each vertex find best combinations of neighbors 
+    for v in g.get_vertices():#[49:50]:
+        n = sorted(g.get_out_neighbors(v))
         if len(n) > 0:
             #split into connected segments
-            combis = list(product(*group_adjacent(sorted(n))))
-            #print(combis)
-            for i,n in enumerate(combis):
-                nn = [w for v in n for w in g.get_out_neighbors(v) if w > v]
-                nn = np.unique(np.concatenate([n, np.array(nn, dtype=int)]))
-                #print(v, n, nn)
-                #make a subgraph for nn
+            vertex_combos = list(product(*group_adjacent(sorted(n))))
+            edge_combos.append([])
+            for i,c in enumerate(vertex_combos):
+                #make a subgraph
                 filt = g.new_vertex_property("bool")
-                filt.a[nn] = True
+                filt.a[[v]+list(c)] = True
                 gg = GraphView(g, vfilt=filt)
                 #check if elements of n in same clique
-                cliques = list(max_cliques(gg))
-                samecli = any(len(np.intersect1d(n, c)) == len(n) for c in cliques)
-                #comps = label_components(gg)[0].fa[np.isin(nn, n).nonzero()[0]]
-                #samecomp = np.all(comps == comps[0])
-                if samecli:
-                    votes.append(np.array(n) - v)
-                    reduced.add_edge_list([(a,b) for i,a in enumerate(n) for b in n[i+1:]])
+                #cliques = list(max_cliques(gg))
+                #samecli = any(len(np.intersect1d(n, c)) == len(n) for c in cliques)
+                #or more simply, rate by how many edges there are
+                edge_combos[-1].append([g.edge_index[e] for e in gg.edges()])
+                edges = g.get_edges([g.edge_index])
+                #if samecli:
+                #    votes.append(np.array(n) - v)
+                #    reduced.add_edge_list([(a,b) for i,a in enumerate(n) for b in n[i+1:]])
                 #graph_draw(gg, vertex_text=g.vertex_index, output_size=(1000, 1000),
                 #    output="results/clean_upp"+str(i)+".pdf")
-    remove_parallel_edges(reduced)
+    seg_combos = []
+    
+    #look for combos with highest number of edges and lowest number of segments
+    for ec in edge_combos:
+        max_num_edges = max([len(c) for c in ec])
+        largest_combos = [c for c in ec if len(c) == max_num_edges]
+        #convert to segment combos
+        segs = [np.unique(seg_index.a[lc]) for lc in largest_combos]
+        min_num_segs = min([len(s) for s in segs])
+        seg_combos.append([s for s in segs if len(s) == min_num_segs])
+        #print(max_num_edges, min_num_segs)
+    
+    #combine into sets
+    sets = []
+    ratings = []
+    for k,sc in enumerate(seg_combos):#[35:37]):
+        if len(sets) == 0:
+            sets = [set(c) for c in sc]
+            ratings = [len(c) for c in sc]
+        else:
+            newsets = []
+            additions = []
+            for c in sc:
+                isects = [s.intersection(c) for s in sets]
+                imax = np.argmax([len(i) for i in isects])
+                max_rating = len(c)
+                for i,isect in enumerate(isects):
+                    if len(isect) == len(c): #subset of existing set
+                        ratings[i] += len(c)
+                    elif len(isect) == len(sets[i]): #superset of existing
+                        max_rating = max(max_rating, ratings[i]+len(c))
+                sets.append(set(c))
+                ratings.append(max_rating)
+                #ADD INTERSECTION TOO??
+        print(k, len(sc), len(sets), len(ratings), max(ratings) if len(ratings) > 0 else 0, sum(ratings) if len(ratings) > 0 else 0)
+    #print(sets[1], ratings)
+    best = np.array(list(sets[np.argmax(ratings)]))
+    #print(seg_index.a[:500])
+    print(sorted(best))
+    alm_indices = seg_index.a[g.get_edges()]
+    #print(np.where(np.isin(edge_indices, best)))
+    #print(g.get_edges()[:10])
+    edges = g.get_edges([seg_index])
+    edges = edges[np.where(np.isin(edges[:,2], best))]
+    #print(edges[:100])
+    reduced.add_edge_list(edges)
     print(reduced)
-    plot_matrix(np.triu(adjacency_matrix(reduced)), "results/clean1.png")
-    graph_draw(reduced, output_size=(1000, 1000), output="results/clean_up1.pdf")
-    votes = [tuple(v) for v in votes]
-    print('counting')
-    print(Counter(votes))
-    # for s in segs[:1]:
-    #     s = [[e[0],e[1]] for e in s]
-    #     print(s)
-    #     filt = g.new_edge_property("bool")
-    #     filt.a = [[e[0],e[1]] not in s for e in g.get_edges()]
-    #     gg = GraphView(g, efilt=filt)
-    #     #print(gg)
-    #     comps = label_components(gg)[0].a
-    #     p = [comps[e[0]] == comps[e[1]] for e in s]
-    #     print(p)
-    #graph_draw(g, output_size=(1000, 1000), output="results/clean_up1.pdf")
+    #remove_parallel_edges(reduced)
+    #reduced.add_edge_list(np.concatenate(edges))
+    #ww = reduced.new_edge_property("float")
+    #ww.a = np.concatenate(weights)
+    #print(reduced)
+    plot_matrix(np.triu(adjacency_matrix(reduced)), "results/clean2.png")
+    #graph_draw(reduced, output_size=(1000, 1000), output="results/clean_up1.pdf")
+    #votes = [tuple(v) for v in votes]
+    #print('counting')
+    #print(Counter(votes))
 
 def structure_graph(msa, alignment_graph, mask_threshold=.5):
     msa = [[int(m[1:]) if len(m) > 0 else -1 for m in a] for a in msa]
