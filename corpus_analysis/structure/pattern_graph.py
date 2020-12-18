91633c957d5d0a7d4d542d7f2f5bf4f0dfd99193
florianthalmann <florianthalmann@bluewin.ch>
2020-12-18 18:20:12 +0900

    refactored sag, tried iterative application

--- corpus_analysis/structure/pattern_graph.py
+++ corpus_analysis/structure/pattern_graph.py
@@ -16,7 +16,7 @@ from ..clusters.histograms import freq_hist_clusters, trans_hist_clusters,\
     freq_trans_hist_clusters
 from ..alignment.smith_waterman import smith_waterman
 
-MIN_VERSIONS = 0.3 #how many of the versions need to contain the patterns
+MIN_VERSIONS = 0.0 #how many of the versions need to contain the patterns
 PARSIM = True
 MIN_SIM = 0.9 #min similarity for non-parsimonious similarity
 COMPS_NOT_BLOCKS = True #use connected components instead of community blocks
@@ -213,115 +213,120 @@ class PatternGraph:
                     adjacency[i][j] = 1
         return adjacency
 
-class SuperAlignmentGraph():
+
+########################################
+
+def super_alignment_graph(sequences, pairings, alignments):
     
-    def __init__(self, sequences, pairings, alignments):
-        self.patterns = create_pattern_dict(sequences, pairings, alignments)
-        self.equivalences = {}
-        self.print('all')
-        #prune pattern dict: keep only ones occurring in a min num of versions
-        self.patterns = {k:v for k,v in self.patterns.items()
-            if len(np.unique([o[1] for o in v])) >= len(sequences)*MIN_VERSIONS}
-        self.print('frequent')
-        # #merge cooccurring patterns
-        # self.merge_patterns(lambda p, q: len(p) == len(q) and
-        #     len(self.patterns[p].intersection(self.patterns[q])) > 0)
-        # self.print('merged cooc')
-        self.merge_patterns(lambda p, q: len(p) == len(q) and
-            all(p[i] == -1 or q[i] == -1 or p[i] == q[i] for i in range(len(p))))
-        self.print('merged equiv')
-        
-        print(sum(len(p) for p in self.patterns.values()))
-        
-        MIN_DIST = 4
-        
-        #catalogue all connections counts between equivalent patterns
-        conns = []
-        print('conns')
-        for pat, occs in list(self.patterns.items()):
-            o = sorted(occs)
-            o = np.array([[o1,o2] for i,o1 in enumerate(o) for o2 in o[i+1:]])
-            o = o[np.logical_or(o[:,0,0] != o[:,1,0],
-                np.absolute(o[:,0,1] - o[:,1,1]) >= MIN_DIST)]
-            r = np.vstack((np.repeat(0, len(pat)), np.arange(0, len(pat)))).T
-            r = np.transpose(np.dstack((r,r)), (0,2,1))
-            conns.append(np.reshape(o[:,None] + r, (len(o)*len(pat),4)))
-        print('tuples')
-        #back to tuples
-        c = np.concatenate(conns)
-        c = c.view(dtype=np.dtype([('x', c.dtype), ('y', c.dtype), ('z', c.dtype), ('u', c.dtype)]))[:,0]
-        print('count', len(c), datetime.datetime.now())
-        edges = Counter(c.tolist())
-        print('sort', len(edges), datetime.datetime.now())
-        bestconns = sorted(edges.items(), key=lambda c: c[1], reverse=True)
-        print(bestconns[:10], datetime.datetime.now())
-        
-        def valid(comp):
-            diffs = np.diff([c for c in comp], axis=0)
-            diffs = np.array([d[1] for d in diffs if d[0] == 0])#same version
-            if len(diffs) > 0:
-                return np.min(diffs[diffs >= 0]) >= MIN_DIST
-            return True
-        
-        #build non-ambiguous graph with strongest connections
-        comps = []
-        locs = {}
-        invalid = set()
-        for pair, count in bestconns:
-            pair = pair[:2], pair[2:]
-            loc1 = locs[pair[0]] if pair[0] in locs else None
-            loc2 = locs[pair[1]] if pair[1] in locs else None
-            if loc1 != None and loc2 != None:
-                if loc1 != loc2 and (loc1, loc2) not in invalid:
-                    #print(loc1, loc2, comps[loc1], comps[loc2])
-                    merged = list(merge(comps[loc1], comps[loc2]))
-                    #merged = sorted(set(comps[loc1] + comps[loc2]))
-                    if valid(merged):
-                        comps[loc1] = merged
-                        for o in comps[loc2]:
-                            locs[o] = loc1
-                        comps[loc2] = [] #keep to not have to update indices
-                    else:
-                        invalid.add((loc1, loc2))
-                        #print(invalid)
-                #else ignore pair
-            elif loc1 != None:
-                pos = next((i for i,c in enumerate(comps[loc1]) if c > pair[1]), len(comps[loc1]))
-                comps[loc1].insert(pos, pair[1])
-                locs[pair[1]] = loc1
-            elif loc2 != None:
-                pos = next((i for i,c in enumerate(comps[loc2]) if c > pair[0]), len(comps[loc2]))
-                comps[loc2].insert(pos, pair[0])
-                locs[pair[0]] = loc2
-            else:
-                locs[pair[0]] = locs[pair[1]] = len(comps)
-                comps.append(list(pair))#pair is ordered
-            #print(loc1, loc2, comps, locs)
-        #remove empty comps
-        comps = [c for c in comps if len(c) > 0]
-        print(len(comps))
-        comps = sorted(comps, key=lambda c: np.median([s[1] for s in c]))
-        
-        typeseqs = [[-1 for e in s] for s in sequences]
-        for i,c in enumerate(comps):
-            for s in c:
-                typeseqs[s[0]][s[1]] = i
-        plot_sequences(typeseqs, 'seqpat.png')
+    patterns = create_pattern_dict(sequences, pairings, alignments)
+    equivalences = {}
+    print_status('all', patterns, equivalences)
+    #prune pattern dict: keep only ones occurring in a min num of versions
+    patterns = {k:v for k,v in patterns.items()
+        if len(np.unique([o[1] for o in v])) >= len(sequences)*MIN_VERSIONS}
+    print_status('frequent', patterns, equivalences)
+    # #merge cooccurring patterns
+    # merge_patterns(lambda p, q: len(p) == len(q) and
+    #     len(patterns[p].intersection(patterns[q])) > 0, patterns, equivalences)
+    # print('merged cooc')
+    merge_patterns(lambda p, q: len(p) == len(q) and
+        all(p[i] == -1 or q[i] == -1 or p[i] == q[i] for i in range(len(p))),
+        patterns, equivalences)
+    print_status('merged equiv', patterns, equivalences)
     
-    def print(self, title):
-        longest3 = [len(l[1]) for l in sorted(list(self.patterns.items()),
-            key=lambda p: len(p[1]), reverse=True)[:3]]
-        print(title, len(self.patterns), len(self.equivalences), longest3)
+    print(sum(len(p) for p in patterns.values()))
     
-    def merge_patterns(self, equiv_func):#lambda p,q: bool
-        merged = {}
-        for p in list(self.patterns.keys()):
-            q = next((q for q in merged.keys() if equiv_func(p, q)), None)
-            if q:
-                merged[q].update(self.patterns[p])
-                if q not in self.equivalences:
-                    self.equivalences[q] = set()
-                self.equivalences[q].add(p)
-            else:
-                merged[p] = self.patterns[p]
-        self.patterns = merged
+    MIN_DIST = 4
+    
+    #catalogue all connections counts between equivalent patterns
+    conns = []
+    print('conns')
+    for pat, occs in list(patterns.items()):
+        o = sorted(occs)
+        o = np.array([[o1,o2] for i,o1 in enumerate(o) for o2 in o[i+1:]])
+        o = o[np.logical_or(o[:,0,0] != o[:,1,0],
+            np.absolute(o[:,0,1] - o[:,1,1]) >= MIN_DIST)]
+        r = np.vstack((np.repeat(0, len(pat)), np.arange(0, len(pat)))).T
+        r = np.transpose(np.dstack((r,r)), (0,2,1))
+        conns.append(np.reshape(o[:,None] + r, (len(o)*len(pat),4)))
+    print('tuples')
+    #back to tuples
+    c = np.concatenate(conns)
+    c = c.view(dtype=np.dtype([('x', c.dtype), ('y', c.dtype), ('z', c.dtype), ('u', c.dtype)]))[:,0]
+    print('count', len(c), datetime.datetime.now())
+    edges = Counter(c.tolist())
+    print('sort', len(edges), datetime.datetime.now())
+    bestconns = sorted(edges.items(), key=lambda c: c[1], reverse=True)
+    print(bestconns[:10], datetime.datetime.now())
+    
+    def valid(comp):
+        diffs = np.diff([c for c in comp], axis=0)
+        diffs = np.array([d[1] for d in diffs if d[0] == 0])#same version
+        if len(diffs) > 0:
+            return np.min(diffs[diffs >= 0]) >= MIN_DIST
+        return True
+    
+    #build non-ambiguous graph with strongest connections
+    comps = []
+    locs = {}
+    invalid = set()
+    for pair, count in bestconns:
+        pair = pair[:2], pair[2:]
+        loc1 = locs[pair[0]] if pair[0] in locs else None
+        loc2 = locs[pair[1]] if pair[1] in locs else None
+        if loc1 != None and loc2 != None:
+            if loc1 != loc2 and (loc1, loc2) not in invalid:
+                #print(loc1, loc2, comps[loc1], comps[loc2])
+                merged = list(merge(comps[loc1], comps[loc2]))
+                #merged = sorted(set(comps[loc1] + comps[loc2]))
+                if valid(merged):
+                    comps[loc1] = merged
+                    for o in comps[loc2]:
+                        locs[o] = loc1
+                    comps[loc2] = [] #keep to not have to update indices
+                else:
+                    invalid.add((loc1, loc2))
+                    #print(invalid)
+            #else ignore pair
+        elif loc1 != None:
+            pos = next((i for i,c in enumerate(comps[loc1]) if c > pair[1]), len(comps[loc1]))
+            comps[loc1].insert(pos, pair[1])
+            locs[pair[1]] = loc1
+        elif loc2 != None:
+            pos = next((i for i,c in enumerate(comps[loc2]) if c > pair[0]), len(comps[loc2]))
+            comps[loc2].insert(pos, pair[0])
+            locs[pair[0]] = loc2
+        else:
+            locs[pair[0]] = locs[pair[1]] = len(comps)
+            comps.append(list(pair))#pair is ordered
+        #print(loc1, loc2, comps, locs)
+    #remove empty comps
+    comps = [c for c in comps if len(c) > 0]
+    print(len(comps))
+    comps = sorted(comps, key=lambda c: np.median([s[1] for s in c]))
+    
+    typeseqs = [[-1 for e in s] for s in sequences]
+    for i,c in enumerate(comps):
+        for s in c:
+            typeseqs[s[0]][s[1]] = i
+    plot_sequences(typeseqs, 'seqpat.png')
+    
+    return [np.array(t) for t in typeseqs]
+
+def print_status(title, patterns, equivalences):
+    longest3 = [len(l[1]) for l in sorted(list(patterns.items()),
+        key=lambda p: len(p[1]), reverse=True)[:3]]
+    print(title, len(patterns), len(equivalences), longest3)
+
+def merge_patterns(equiv_func, patterns, equivalences={}):#lambda p,q: bool
+    merged = {}
+    for p in list(patterns.keys()):
+        q = next((q for q in merged.keys() if equiv_func(p, q)), None)
+        if q:
+            merged[q].update(patterns[p])
+            if q not in equivalences:
+                equivalences[q] = set()
+            equivalences[q].add(p)
+        else:
+            merged[p] = patterns[p]
+    return merged, equivalences
