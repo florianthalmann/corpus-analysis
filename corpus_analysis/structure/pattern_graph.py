78b777f4ce68885652df77242c2b20c87fba0e59
florianthalmann <florianthalmann@bluewin.ch>
2020-12-24 17:09:37 +0900

    fixed pattern graph, improved super alignment graph

--- corpus_analysis/structure/pattern_graph.py
+++ corpus_analysis/structure/pattern_graph.py
@@ -11,19 +11,21 @@ from scipy.sparse.csgraph import connected_components
 import graph_tool.all as gt
 from graph_tool.all import Graph, GraphView, graph_draw
 from .graphs import graph_from_matrix
+from .hierarchies import get_hierarchy_labels, get_most_salient_labels
 from ..util import plot_sequences, mode, flatten, group_by
 from ..clusters.histograms import freq_hist_clusters, trans_hist_clusters,\
     freq_trans_hist_clusters
 from ..alignment.smith_waterman import smith_waterman
 
-MIN_VERSIONS = 0.03 #how many of the versions need to contain the patterns
+MIN_VERSIONS = 0.1 #how many of the versions need to contain the patterns
 PARSIM = True
+PARSIM_DIFF = 0 #the largest allowed difference in parimonious mode (full containment == 0)
 MIN_SIM = 0.9 #min similarity for non-parsimonious similarity
 COMPS_NOT_BLOCKS = True #use connected components instead of community blocks
 
 def similarity(match, p1, p2, parsim):
     minlen = min(len(p1), len(p2))
-    if parsim: return 1 if abs(match - minlen) <= 0 else 0
+    if parsim: return 1 if abs(match - minlen) == PARSIM_DIFF else 0
     return match / minlen
 
 def sw_similarity(p1, p2, parsim=PARSIM):
@@ -38,6 +40,7 @@ def get_occ_matrix(pattern, dict, num_seqs, max_len):
         matrix[occ[0], np.arange(occ[1], occ[1]+len(pattern))] = 1
     return matrix
 
+#finds the longest overlap between any two occurrences of the patterns
 def cooc_similarity(p1, p2, occmat1, occmat2, parsim=PARSIM):
     both = np.logical_and(occmat1, occmat2)
     diffs = np.transpose(np.diff(np.where(both == 1)))
@@ -47,7 +50,6 @@ def cooc_similarity(p1, p2, occmat1, occmat2, parsim=PARSIM):
 def group_by_comps(patterns, adjacency_matrix):
     comps = connected_components(adjacency_matrix)[1]
     groups = group_by(range(len(patterns)), lambda i: comps[i])
-    print(groups)
     return [[patterns[i] for i in g] for g in groups]
 
 #group by a transitive distance
@@ -58,6 +60,10 @@ def group_by_sim2(patterns, simfunc, minsim):
 
 def group_by_sim(groups, simfunc, minsim):
     return [subg for g in groups for subg in group_by_sim2(g, simfunc, minsim)]
+
+def refine_adjacency(adjacency, simfunc, patterns):
+    return np.array([[simfunc(p1, p2) if j > i and adjacency[i,j] else 0
+        for j, p2 in enumerate(patterns)] for i, p1 in enumerate(patterns)])
 
 def cluster(patterns, relative):
     #prepared = np.array([np.array(p)+1 for p in patterns])#to positive integers
@@ -101,19 +107,20 @@ class PatternGraph:
         self.print('merged equiv')
         #print(sorted(list(self.patterns.items()), key=lambda p: len(p[0])*sqrt(len(p[1])), reverse=True)[0:10])
         
-        groups = [list(self.patterns.keys())]
+        patterns = list(self.patterns.keys())
+        groups = [patterns]
         
-        #cluster relative
-        groups = [c for g in groups for c in cluster(g, False)[0]]
-        print('clusters', [len(g) for g in groups])
-        print('clusters', [sum([len(p) for p in g]) for g in groups])
-        groups = self.keep_largest_pattern_groups(groups, 10)
-        groups = groups[0:5]
-        [print(g[:5]) for g in groups]
-        
-        ps = groups[0]
-        ref = self.patterns[ps[0]]
-        [print(self.patterns[ps[i]]) for i in range(0,len(ps))]
+        # #cluster relative
+        # groups = [c for g in groups for c in cluster(g, False)[0]]
+        # print('clusters', [len(g) for g in groups])
+        # print('clusters', [sum([len(p) for p in g]) for g in groups])
+        # groups = self.keep_largest_pattern_groups(groups, 10)
+        # #groups = groups[:5]
+        # [print(g[:5]) for g in groups]
+        # 
+        # ps = groups[0]
+        # ref = self.patterns[ps[0]]
+        # [print(self.patterns[ps[i]]) for i in range(0,len(ps))]
         
         # #cluster absolute
         # groups = [c for g in groups for c in cluster(g, True)[0]]
@@ -125,29 +132,33 @@ class PatternGraph:
         # 
         # [print([e for e in g]) for g in groups]
         
-        # patterns = list(self.patterns.keys())
-        # compatible = np.ones((len(patterns),len(patterns)))
-        # 
-        # #cooccurrence similarity
-        # num_seqs = len(sequences)
-        # max_len = max([len(s) for s in sequences])
-        # occ_matrices = [get_occ_matrix(p, self.patterns, num_seqs, max_len)
-        #     for p in patterns]
-        # compatible = np.array([[cooc_similarity(p1, p2, occ_matrices[i], occ_matrices[j])
-        #     if j > i and compatible[i,j] else 0
-        #     for j, p2 in enumerate(patterns)] for i, p1 in enumerate(patterns)])
-        # print('cooc', len(np.nonzero(compatible)[0]))
+        adjacency = self.get_adjacency_matrix(groups)
         
-        # #content similarity
-        # sorted_patterns = list([np.sort(p) for p in patterns])
-        # compatible = group_by_sim(isect_similarity, sorted_patterns, compatible)
+        #cooccurrence similarity
+        num_seqs = len(sequences)
+        max_len = max([len(s) for s in sequences])
+        occ_matrices = [get_occ_matrix(p, self.patterns, num_seqs, max_len)
+            for p in patterns]
+        adjacency = np.array([[cooc_similarity(p1, p2, occ_matrices[i], occ_matrices[j])
+            if j > i and adjacency[i,j] else 0
+            for j, p2 in enumerate(patterns)] for i, p1 in enumerate(patterns)])
+        print('cooc', len(np.nonzero(adjacency)[0]))
         
-        # #sequence similarity
-        # compatible = group_by_sim(sw_similarity, patterns, compatible)
+        #content similarity
+        sorted_patterns = list([np.sort(p) for p in patterns])
+        adjacency = refine_adjacency(adjacency, isect_similarity, sorted_patterns)
+        print('cont', len(np.nonzero(adjacency)[0]))
+        
+        #sequence similarity
+        adjacency = refine_adjacency(adjacency, sw_similarity, patterns)
+        print('seqs', len(np.nonzero(adjacency)[0]))
+        
+        groups = group_by_comps(patterns, adjacency)
+        groups = self.keep_most_frequent_pattern_groups(groups, 8)
         
         #make weighted adjacency matrix
-        adjacency = self.get_adjacency_matrix(groups)
         patterns = list(self.patterns.keys())
+        adjacency = self.get_adjacency_matrix(groups)
         pattern_counts = np.array([len(self.patterns[p]) for p in patterns])
         adjacency *= np.minimum.outer(pattern_counts, pattern_counts)
         #make graph and find communities
@@ -173,7 +184,7 @@ class PatternGraph:
             for j,k in self.patterns[p]:
                 for l in range(k, k+len(p)):
                     typeseqs[j][l] += [b]
-        typeseqs = [[mode(e) if len(e) > 0 else 0 for e in s] for s in typeseqs]
+        typeseqs = [[mode(e) if len(e) > 0 else -1 for e in s] for s in typeseqs]
         plot_sequences(typeseqs, 'seqpats2.png')
     
     def print(self, title):
@@ -202,6 +213,14 @@ class PatternGraph:
             if k in flatten(largest, 1)}
         return largest
     
+    def keep_most_frequent_pattern_groups(self, groups, count, length_based=True):
+        key = lambda c: sum([len(self.patterns[p]) for p in c]) if length_based \
+            else lambda c: len(c)
+        largest = sorted(groups, key=key, reverse=True)[:count]
+        self.patterns = {k:v for k,v in self.patterns.items()
+            if k in flatten(largest, 1)}
+        return largest
+    
     def get_adjacency_matrix(self, groups):
         patterns = list(self.patterns.keys())
         indices = {p:i for i,p in enumerate(patterns)}
@@ -217,7 +236,7 @@ class PatternGraph:
 ########################################
 
 def super_alignment_graph(sequences, pairings, alignments):
-    
+    plot_sequences(sequences, 'seqpat.png')
     patterns = create_pattern_dict(sequences, pairings, alignments)
     equivalences = {}
     print_status('all', patterns, equivalences)
@@ -233,12 +252,17 @@ def super_alignment_graph(sequences, pairings, alignments):
         all(p[i] == -1 or q[i] == -1 or p[i] == q[i] for i in range(len(p))),
         patterns, equivalences)
     print_status('merged equiv', patterns, equivalences)
+    #remove uniform (one value or blank)
+    uniq = {k:len(np.unique(list(k))) for k in patterns.keys()}
+    patterns = {k:v for k,v in patterns.items() if uniq[k] > 2 or (uniq[k] == 2 and -1 not in k)}
+    print_status('removed uniform', patterns, equivalences)
     
+    print([p[0] for p in sorted(patterns.items(), key=lambda p: len(p[1]), reverse=True)[:5]])
     print(sum(len(p) for p in patterns.values()))
     
     MIN_DIST = 4
     
-    #catalogue all connections counts between equivalent patterns
+    #catalogue all connection counts between equivalent patterns
     conns = []
     print('conns')
     for pat, occs in list(patterns.items()):
@@ -305,22 +329,26 @@ def super_alignment_graph(sequences, pairings, alignments):
     comps = sorted(comps, key=lambda c: np.median([s[1] for s in c]))
     print(len(comps))
     #merge compatible adjacent
-    merged = comps[:1]
-    for i,c in enumerate(comps[1:], 1):
-        m = list(merge(comps[i-1], c))
-        if valid(m):
-            merged[-1] = m
-        else:
-            merged.append(c)
+    # merged = comps[:1]
+    # for i,c in enumerate(comps[1:], 1):
+    #     m = list(merge(comps[i-1], c))
+    #     if valid(m):
+    #         merged[-1] = m
+    #     else:
+    #         merged.append(c)
+    # 
+    # print(len(merged))
+    # comps = merged
     
-    print(len(merged))
-    comps = merged
-    
-    typeseqs = [[-1 for e in s] for s in sequences]
+    typeseqs = [np.repeat(-1, len(s)) for s in sequences]
     for i,c in enumerate(comps):
         for s in c:
             typeseqs[s[0]][s[1]] = i
-    plot_sequences(typeseqs, 'seqpat.png')
+    plot_sequences(typeseqs.copy(), 'seqpat..png')
+    
+    #typeseqs = [l[-2] for l in get_hierarchy_labels(typeseqs)]
+    typeseqs = get_most_salient_labels(typeseqs, 30, [-1])
+    plot_sequences(typeseqs, 'seqpat...png')
     
     return [np.array(t) for t in typeseqs]
 
