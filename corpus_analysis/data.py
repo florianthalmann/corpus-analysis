70567928c068ef232982cbe616239ca28f1f161f
florianthalmann <florianthalmann@bluewin.ch>
2021-11-29 13:52:51 +0900

    improved dataframe concurrency

--- corpus_analysis/data.py
+++ corpus_analysis/data.py
@@ -1,6 +1,7 @@
 import os, time
 import numpy as np
 import pandas as pd
+from .util import flatten
 
 class Data:
     def __init__(self, path, columns):
@@ -10,11 +11,15 @@ class Data:
     def read(self):
         if self.path and os.path.isfile(self.path):
             try:
-                return pd.read_csv(self.path)
-            except pd.errors.EmptyDataError:
-                time.sleep(0.01)
-                return self.read()
+                df = pd.read_csv(self.path)
+                return (df if len(df) > 0 else self.reread())#for concurrency
+            except pd.errors.EmptyDataError:#for concurrency
+                return self.reread()
         return pd.DataFrame([], columns=self.columns)
+    
+    def reread(self):
+        time.sleep(0.01)
+        return self.read()
     
     def rows_exist(self, rows):
         df = self.read()
@@ -22,13 +27,14 @@ class Data:
     
     def get_rows(self, rows):
         df = self.read()
-        return [df.iloc[np.where((df[df.columns[:len(rows[0])]] == r).all(1))].to_numpy()[0]
+        rows = [df.iloc[np.where((df[df.columns[:len(r)]] == r).all(1))].to_numpy()
             for r in rows]
+        return [r[0] for r in rows if len(r) > 0]
     
     #lazy: calls rows_func only if no rows beginning with ref_rows exist
     def add_rows(self, ref_rows, rows_func):
         if not self.rows_exist(ref_rows):
-            rows =  rows_func()
+            rows = rows_func()
             data = self.read().append(pd.DataFrame(rows, columns=self.columns),
                 ignore_index=True)
             if self.path: data.to_csv(self.path, index=False)
