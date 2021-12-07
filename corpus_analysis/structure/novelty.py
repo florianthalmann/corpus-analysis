7005dd183fcd28d73ab9e18441262774d7ab8df1
florianthalmann <florianthalmann@bluewin.ch>
2021-12-07 16:50:55 +0900

    novelty

---
+++ corpus_analysis/structure/novelty.py
@@ -0,0 +1,95 @@
+#code from https://www.audiolabs-erlangen.de/resources/MIR/FMP/C4/C4S4_NoveltySegmentation.html
+import numpy as np
+from scipy import signal
+from scipy.ndimage import filters
+from ..util import plot
+
+def get_novelty_boundaries(S, kernel_size=40, min_dist=15):
+    novelty = compute_novelty_ssm(S, L=kernel_size, exclude=True)
+    #return signal.find_peaks(novelty, distance=min_dist)[0]
+    return peak_picking_MSAF(novelty)[0]
+
+def peak_picking_MSAF(x, median_len=16, offset_rel=0.05, sigma=4.0):
+    """Peak picking strategy following MSFA using an adaptive threshold (https://github.com/urinieto/msaf)
+
+    Notebook: C6/C6S1_PeakPicking.ipynb
+
+    Args:
+        x (np.ndarray): Input function
+        median_len (int): Length of media filter used for adaptive thresholding (Default value = 16)
+        offset_rel (float): Additional offset used for adaptive thresholding (Default value = 0.05)
+        sigma (float): Variance for Gaussian kernel used for smoothing the novelty function (Default value = 4.0)
+
+    Returns:
+        peaks (np.ndarray): Peak positions
+        x (np.ndarray): Local threshold
+        threshold_local (np.ndarray): Filtered novelty curve
+    """
+    offset = x.mean() * offset_rel
+    x = filters.gaussian_filter1d(x, sigma=sigma)
+    threshold_local = filters.median_filter(x, size=median_len) + offset
+    peaks = []
+    for i in range(1, x.shape[0] - 1):
+        if x[i - 1] < x[i] and x[i] > x[i + 1]:
+            if x[i] > threshold_local[i]:
+                peaks.append(i)
+    peaks = np.array(peaks)
+    return peaks, x, threshold_local
+
+def compute_novelty_ssm(S, kernel=None, L=10, var=0.5, exclude=False):
+    """Compute novelty function from SSM [FMP, Section 4.4.1]
+
+    Notebook: C4/C4S4_NoveltySegmentation.ipynb
+
+    Args:
+        S (np.ndarray): SSM
+        kernel (np.ndarray): Checkerboard kernel (if kernel==None, it will be computed) (Default value = None)
+        L (int): Parameter specifying the kernel size M=2*L+1 (Default value = 10)
+        var (float): Variance parameter determing the tapering (epsilon) (Default value = 0.5)
+        exclude (bool): Sets the first L and last L values of novelty function to zero (Default value = False)
+
+    Returns:
+        nov (np.ndarray): Novelty function
+    """
+    if kernel is None:
+        kernel = compute_kernel_checkerboard_gaussian(L=L, var=var)
+    N = S.shape[0]
+    M = 2*L + 1
+    nov = np.zeros(N)
+    # np.pad does not work with numba/jit
+    S_padded = np.pad(S, L, mode='constant')
+
+    for n in range(N):
+        # Does not work with numba/jit
+        nov[n] = np.sum(S_padded[n:n+M, n:n+M] * kernel)
+    if exclude:
+        right = np.min([L, N])
+        left = np.max([0, N-L])
+        nov[0:right] = 0
+        nov[left:N] = 0
+
+    return nov
+
+def compute_kernel_checkerboard_gaussian(L, var=1, normalize=True):
+    """Compute Guassian-like checkerboard kernel [FMP, Section 4.4.1].
+    See also: https://scipython.com/blog/visualizing-the-bivariate-gaussian-distribution/
+
+    Notebook: C4/C4S4_NoveltySegmentation.ipynb
+
+    Args:
+        L (int): Parameter specifying the kernel size M=2*L+1
+        var (float): Variance parameter determing the tapering (epsilon) (Default value = 1.0)
+        normalize (bool): Normalize kernel (Default value = True)
+
+    Returns:
+        kernel (np.ndarray): Kernel matrix of size M x M
+    """
+    taper = np.sqrt(1/2) / (L * var)
+    axis = np.arange(-L, L+1)
+    gaussian1D = np.exp(-taper**2 * (axis**2))
+    gaussian2D = np.outer(gaussian1D, gaussian1D)
+    kernel_box = np.outer(np.sign(axis), np.sign(axis))
+    kernel = kernel_box * gaussian2D
+    if normalize:
+        kernel = kernel / np.sum(np.abs(kernel))
+    return kernel
